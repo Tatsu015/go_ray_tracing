61ded1535cbd1ff29d2c1b887a81eb20ea4e48ef
Tatsu015 <tatsumi.soichiro@gmail.com>
2023-06-12 00:11:23 +0900

    add Vec3 math operations

--- vec3.go
+++ vec3.go
@@ -1,7 +1,57 @@
 package main
 
+import "math"
+
 type Vec3 struct {
-	x int
-	y int
-	z int
+	x float64
+	y float64
+	z float64
+}
+
+func add(u Vec3, v Vec3) Vec3 {
+	return Vec3{
+		x: u.x + v.x,
+		y: u.y + v.y,
+		z: u.z + v.z,
+	}
+}
+
+func sub(u Vec3, v Vec3) Vec3 {
+	return Vec3{
+		x: u.x - v.x,
+		y: u.y - v.y,
+		z: u.z - v.z,
+	}
+}
+
+func mul(u Vec3, v Vec3) Vec3 {
+	return Vec3{
+		x: u.x * v.x,
+		y: u.y * v.y,
+		z: u.z * v.z,
+	}
+}
+
+func times(u Vec3, t float64) Vec3 {
+	return Vec3{
+		x: u.x * t,
+		y: u.x * t,
+		z: u.x * t,
+	}
+}
+
+func div(u Vec3, t float64) Vec3 {
+	return Vec3{
+		x: u.x / t,
+		y: u.y / t,
+		z: u.z / t,
+	}
+}
+
+func (v Vec3) length() float64 {
+	return math.Sqrt(v.length_squared())
+}
+
+func (v Vec3) length_squared() float64 {
+	return v.x*v.x + v.y*v.y + v.z*v.z
 }
