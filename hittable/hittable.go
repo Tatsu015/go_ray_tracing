8eb1cb985ce1661e28538a794dadf2910ced8ac3
Tatsu015 <tatsumi.soichiro@gmail.com>
2023-06-17 14:41:07 +0900

    add hittable

---
+++ hittable/hittable.go
@@ -0,0 +1,16 @@
+package hittable
+
+import (
+	"github.com/Tatsu015/go_ray_tracing.git/raytrace"
+	"github.com/Tatsu015/go_ray_tracing.git/vec"
+)
+
+type HitRecord struct {
+	p      vec.Point
+	normal vec.Vec3
+	t      float64
+}
+
+type Hittable interface {
+	Hit(ray *raytrace.Ray, tMin float64, tMax float64, record HitRecord) bool
+}
