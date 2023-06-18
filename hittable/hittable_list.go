3f8e2e3f9472559e858859b66fcc11a09fd36357
Tatsu015 <tatsumi.soichiro@gmail.com>
2023-06-18 21:04:51 +0900

    add hittable list

---
+++ hittable/hittable_list.go
@@ -0,0 +1,31 @@
+package hittable
+
+import "github.com/Tatsu015/go_ray_tracing.git/raytrace"
+
+type HittableList struct {
+	hittables []Hittable
+}
+
+func (h *HittableList) Add(hittable Hittable) {
+	h.hittables = append(h.hittables, hittable)
+}
+
+func (h *HittableList) Hit(ray *raytrace.Ray, tMin float64, tMax float64) *HitRecord {
+	hitAnythig := false
+	closeSoFar := tMax
+	hitRecord := DefaultHitRecord()
+
+	for _, v := range h.hittables {
+		rec := v.Hit(ray, tMin, closeSoFar)
+		if rec != nil {
+			hitAnythig = true
+			closeSoFar = rec.t
+			hitRecord = *rec
+		}
+
+	}
+	if hitAnythig {
+		return &hitRecord
+	}
+	return nil
+}
